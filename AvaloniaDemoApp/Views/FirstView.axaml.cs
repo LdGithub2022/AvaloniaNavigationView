d6518fcc7cfade45cb93d06b17fe57e3d30b5041
LdGithub2022 <113286078+LdGithub2022@users.noreply.github.com>
2022-09-11 11:13:41 +0100

    Create FirstView.axaml.cs

---
+++ AvaloniaDemoApp/Views/FirstView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace AvaloniaDemoApp.Views
+{
+    public partial class FirstView : UserControl
+    {
+        public FirstView()
+        {
+            InitializeComponent();
+        }
+    }
+}
